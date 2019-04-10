4d77df2ef2eaac55b7ca3024d23f862309ea936e
Mendy <Mendelmullerpupa@gmail.com>
2019-04-09 23:19:04 -0400

    Nothing new

--- Homework 4_8_2019/Scripts/Index.js
+++ Homework 4_8_2019/Scripts/Index.js
@@ -4,41 +4,46 @@
 	});
 
 	$("#submit").click(() => {
-		$.post('/home/AddPerson', { FirstName: $("#mdl-fn").val(), LastName: $("#mdl-ln").val(), Age: $("#mdl-age").val() }, function (people) {
-			RefreshTable(people);
-		});
-	});
-
-	$("#edit").click(() => {
-		$.post('/home/EditPerson', { Id: $("#mdl-id").val(), FirstName: $("#mdl-fn").val(), LastName: $("#mdl-ln").val(), Age: $("#mdl-age").val() }, function (people) {
-			RefreshTable(people);
-		});
+		if ($("#submit").val() === `submit`) {
+			$.post('/home/AddPerson', { FirstName: $("#mdl-fn").val(), LastName: $("#mdl-ln").val(), Age: $("#mdl-age").val() }, function (people) {
+				RefreshTable(people);
+			});
+		}
+		else if ($("#submit").val() === `edit`) {
+			//$("#edit").click(() => {
+				$.post('/home/EditPerson', { Id: $("#mdl-id").val(), FirstName: $("#mdl-fn").val(), LastName: $("#mdl-ln").val(), Age: $("#mdl-age").val() }, function (people) {
+					RefreshTable(people);
+				});
+			//});
+		} 
 	});
 
 	$("#addperson").click(() => {
 		$("#mdl-fn").val(``);
 		$("#mdl-ln").val(``);
 		$("#mdl-age").val(``);
-		//$(".submit").val(`Submit`);
+		$("#submit").val('Submit');
+		$("#submit").attr('class', 'btn btn-primary');
 		$("#personmng").modal();
 	});
 
-	$("#editperson").click(() => {
+	//$("#editperson").click(() => {
 		
-		const id = $(this).data('personid');
-		const firstName = $(this).data('fn');
-		const lastName = $(this).data('ln');
-		const age = $(this).data('age');
+	//	const id = $(this).data('personid');
+	//	const firstName = $(this).data('fn');
+	//	const lastName = $(this).data('ln');
+	//	const age = $(this).data('age');
 
-		$("#mdl-id").val(id);
-		$("#mdl-fn").val(firstName);
-		$("#mdl-ln").val(lastName);
-		$("#mdl-age").val(age);
+	//	$("#mdl-id").val(id);
+	//	$("#mdl-fn").val(firstName);
+	//	$("#mdl-ln").val(lastName);
+	//	$("#mdl-age").val(age);
 
-		$(".submit").val(`Edit`);
-		$("#submit-form").attr("data-action", "edit");
-		$("#personmng").modal();
-	});
+	//	$("#submit").val(`Edit`);
+	//	$("#submit").attr('class', 'btn btn-success');
+	//	$("#submit-form").attr("data-action", "edit");
+	//	$("#personmng").modal();
+	//});
 
 	//function GetModalPerson() {
 	//   const Person = {
@@ -62,8 +67,25 @@
                                         <td>${Person.Age}</td>
 										<td>
 										<button class="btn btn-danger" data-id="${Person.Id}">Delete</button>
-										<button class="btn btn-primary" id="editperson" data-personid="${Person.Id}" data-fn="${Person.FirstName}" data-ln="${Person.LastName}" data-age="${Person.Age}">Edit</button>
+										<button class="btn btn-primary editperson" /*id="editperson${Person.Id}"*/ data-personid="${Person.Id}" data-fn="${Person.FirstName}" data-ln="${Person.LastName}" data-age="${Person.Age}">Edit</button>
 										</td>
                                  </tr>`);
 	}
+	$(".editperson").click(() => {
+
+		const id = $(this).data('personid');
+		const firstName = $(this).data('fn');
+		const lastName = $(this).data('ln');
+		const age = $(this).data('age');
+
+		$("#mdl-id").val(id);
+		$("#mdl-fn").val(firstName);
+		$("#mdl-ln").val(lastName);
+		$("#mdl-age").val(age);
+
+		$("#submit").val("Edit");
+		$("#submit").attr('class', 'btn btn-success');
+		$("#submit-form").attr("data-action", "edit");
+		$("#personmng").modal();
+	});
 });
