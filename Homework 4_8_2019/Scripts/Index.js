358ed9dc59949afa10e2f8a8351ad35c40de64ae
Mendy <Mendelmullerpupa@gmail.com>
2019-04-09 23:47:18 -0400

    Complete bh

--- Homework 4_8_2019/Scripts/Index.js
+++ Homework 4_8_2019/Scripts/Index.js
@@ -4,25 +4,26 @@
 	});
 
 	$("#submit").click(() => {
-		if ($("#submit").val() === `submit`) {
+		if ($("#submit").text() === "submit") {
 			$.post('/home/AddPerson', { FirstName: $("#mdl-fn").val(), LastName: $("#mdl-ln").val(), Age: $("#mdl-age").val() }, function (people) {
 				RefreshTable(people);
 			});
 		}
-		else if ($("#submit").val() === `edit`) {
+		else if ($("#submit").text() === "Edit") {
 			//$("#edit").click(() => {
 				$.post('/home/EditPerson', { Id: $("#mdl-id").val(), FirstName: $("#mdl-fn").val(), LastName: $("#mdl-ln").val(), Age: $("#mdl-age").val() }, function (people) {
 					RefreshTable(people);
 				});
 			//});
-		} 
+		}
+		$("#personmng").modal('hide');
 	});
 
 	$("#addperson").click(() => {
 		$("#mdl-fn").val(``);
 		$("#mdl-ln").val(``);
 		$("#mdl-age").val(``);
-		$("#submit").val("Submit");
+		$("#submit").text('submit');
 		$("#submit").attr('class', 'btn btn-primary');
 		$("#personmng").modal();
 	});
@@ -67,18 +68,18 @@
                                         <td>${Person.Age}</td>
 										<td>
 										<button class="btn btn-danger delete" data-id="${Person.Id}">Delete</button>
-										<button class="btn btn-primary editperson" /*id="editperson${Person.Id}"*/ data-personid="${Person.Id}" data-fn="${Person.FirstName}" data-ln="${Person.LastName}" data-age="${Person.Age}">Edit</button>
+										<button class="btn btn-primary editperson" data-id="${Person.Id}" data-fn="${Person.FirstName}" data-ln="${Person.LastName}" data-age="${Person.Age}">Edit</button>
 										</td>
                                  </tr>`);
 	}
 	$("#people-tbl").on('click', '.delete', function () {
-		$.post('/home/DeletePerson', { id: $(this).data('personid') }, function (people) {
+		$.post('/home/DeletePerson', { id: $(this).data('id') }, function (people) {
 			RefreshTable(people);
 		});
 	});
 	$("#people-tbl").on('click', '.editperson', function () {
 
-		const id = $(this).data('personid');
+		const id = $(this).data('id');
 		const firstName = $(this).data('fn');
 		const lastName = $(this).data('ln');
 		const age = $(this).data('age');
@@ -88,7 +89,7 @@
 		$("#mdl-ln").val(lastName);
 		$("#mdl-age").val(age);
 
-		$("#submit").val("Edit");
+		$("#submit").text('Edit');
 		$("#submit").attr('class', 'btn btn-success');
 		$("#submit-form").attr("data-action", "edit");
 		$("#personmng").modal();
